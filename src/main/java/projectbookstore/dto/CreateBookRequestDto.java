79dbb6d67f315e9c22ed51bf5b1c9731e548d0a7
Hanna Ratushniak <ratushnyakhanna@gmail.com>
2024-05-02 14:11:34 +0300

    added @NotBlank annotation

--- src/main/java/projectbookstore/dto/CreateBookRequestDto.java
+++ src/main/java/projectbookstore/dto/CreateBookRequestDto.java
@@ -1,7 +1,9 @@
 package projectbookstore.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,15 +11,21 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CreateBookRequestDto {
+    @NotBlank
     @NotNull
     private String title;
+    @NotBlank
     @NotNull
     private String author;
     @NotNull
+    @Size(min = 13)
+    @NotBlank
     private String isbn;
     @NotNull
     @Min(0)
     private BigDecimal price;
+    @NotBlank
     private String description;
+    @NotBlank
     private String coverImage;
 }
