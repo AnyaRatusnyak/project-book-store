bb798aa01d0b34f33066d098ba1a34aee356d724
Hanna Ratushniak <ratushnyakhanna@gmail.com>
2024-05-15 00:13:01 +0300

    Implementation JWT support

--- src/main/java/projectbookstore/controller/AuthenticationController.java
+++ src/main/java/projectbookstore/controller/AuthenticationController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import projectbookstore.dto.UserLoginRequestDto;
+import projectbookstore.dto.UserLoginResponseDto;
 import projectbookstore.dto.UserRegistrationRequestDto;
 import projectbookstore.dto.UserResponseDto;
 import projectbookstore.exception.RegistrationException;
+import projectbookstore.security.AuthenticationService;
 import projectbookstore.service.UserService;
 
 @RequiredArgsConstructor
@@ -16,11 +19,16 @@ import projectbookstore.service.UserService;
 @RequestMapping(value = "/api/auth")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     public UserResponseDto register(@Valid @RequestBody UserRegistrationRequestDto request)
             throws RegistrationException {
         return userService.register(request);
+    }
 
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
     }
 }
---
+++ src/main/java/projectbookstore/dto/UserLoginResponseDto.java
@@ -0,0 +1,4 @@
+package projectbookstore.dto;
+
+public record UserLoginResponseDto(String token) {
+}
