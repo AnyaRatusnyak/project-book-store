99b77adc2b1ce5bb9ec9647846b1b4f956d48880
Hanna Ratushniak <ratushnyakhanna@gmail.com>
2024-05-16 00:42:22 +0300

    Added changes according the review

--- src/main/java/projectbookstore/dto/UserLoginRequestDto.java
+++ src/main/java/projectbookstore/dto/UserLoginRequestDto.java
@@ -2,9 +2,9 @@ package projectbookstore.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
@@ -13,7 +13,7 @@ public class UserLoginRequestDto {
     @NotBlank
     private String email;
     @NotBlank
-    @Length(min = 8, max = 20)
+    @Size(min = 8, max = 20)
     private String password;
 
 }
