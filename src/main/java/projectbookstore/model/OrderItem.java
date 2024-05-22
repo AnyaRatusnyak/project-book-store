95950eb35833cd431759632ceb55540125322d2e
Hanna Ratushniak <ratushnyakhanna@gmail.com>
2024-05-22 15:45:31 +0300

    Added Order and OrderItem entities

---
+++ src/main/java/projectbookstore/model/OrderItem.java
@@ -0,0 +1,23 @@
+package projectbookstore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
