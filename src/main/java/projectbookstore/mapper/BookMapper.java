78782bf51b9557d7570c7d1d092d49d90780476c
Hanna Ratushniak <ratushnyakhanna@gmail.com>
2024-05-22 19:54:06 +0300

    changes according to review part3

--- src/main/java/projectbookstore/mapper/BookMapper.java
+++ src/main/java/projectbookstore/mapper/BookMapper.java
@@ -12,8 +12,6 @@ import projectbookstore.config.MapperConfig;
 import projectbookstore.dto.book.BookDto;
 import projectbookstore.dto.book.BookDtoWithoutCategoryIds;
 import projectbookstore.dto.book.CreateBookRequestDto;
-import projectbookstore.dto.book.BookDto;
-import projectbookstore.dto.book.CreateBookRequestDto;
 import projectbookstore.model.Book;
 import projectbookstore.model.Category;
 
