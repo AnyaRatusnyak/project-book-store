8acd9c4727996c74c1084bc1ee52b03a89ba46c2
Hanna Ratushniak <96320551+AnyaRatusnyak@users.noreply.github.com>
2024-05-29 11:53:09 +0300

    Docker hw (#12)
    * Added Category Entity, Controler,Mapper,Dto
    
    * Added PUT and DELETE endpoints
    
    * Added GET books by category endpoints
    
    * Changed  CreateBookRequestDto
    
    * Added shopping cart with cart items
    
    * added changes after rebase
    
    * added Docker
    
    * added .env to gitignore
    
    * Remove .env from repository and add to .gitignore
    
    * added changes according the review
    
    * added changes according the review 2
    
    ---------
    
    Co-authored-by: Hanna Ratushniak <ratushnyakhanna@gmail.com>

--- src/main/java/projectbookstore/dto/category/CreateCategoryRequestDto.java
+++ src/main/java/projectbookstore/dto/category/CreateCategoryRequestDto.java
@@ -1,13 +1,11 @@
 package projectbookstore.dto.category;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateCategoryRequestDto {
     @NotBlank
-    @NotNull
     private String name;
     @NotBlank
     private String description;
